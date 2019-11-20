210b8c331007330cb37693a7a2f57412246faca2
Toumtou <Fbkaba@gmail.com>
2019-11-20 16:19:44 +0100

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'File Exchange';
+  description = 'Tool to upload and view files on server';
+}
